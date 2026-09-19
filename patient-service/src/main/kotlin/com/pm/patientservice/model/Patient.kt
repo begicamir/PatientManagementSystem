ed033e7d6c15839870cc1bc9d8f7ed72c80f1ea8
begicamir <amirbegicdev@gmail.com>
2026-09-20 00:39:40 +0200

    Create patient service .yaml file for in-memory DB with a Patient.kt entity. Update pom.xml to allow h2 DB and h2-console

---
+++ patient-service/src/main/kotlin/com/pm/patientservice/model/Patient.kt
@@ -0,0 +1,36 @@
+package com.pm.patientservice.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDate
+
+@Entity
+class Patient(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    var firstName: String,
+
+    @Column(nullable = false)
+    var lastName: String,
+
+    @Email
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(nullable = false)
+    var address: String,
+
+    @Column(nullable = false)
+    var dateOfBirth: LocalDate,
+
+    @Column(nullable = false)
+    var registeredDate: LocalDate
+)
